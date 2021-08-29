a82469b11b202c26fa282f445aa4379532691319
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-08-29 14:59:24 +0200

    Update joix_startup.sh

--- joix_startup.sh
+++ joix_startup.sh
@@ -23,6 +23,16 @@ mkdir $rootconfigfolder
 chown -R $uid:$gid $rootdatafolder
 chown -R $uid:$gid $rootconfigfolder
 
+printf "%s" "Waiting for connexion"
+while ! ping -c 1 -n -w 1 1.1.1.1 &> /dev/null
+do
+    printf "%c" "."
+done
+printf "\n%s\n"  "Online!"
+
+# Getting joix.yml
+wget -O $rootconfigfolder/joix.yml https://raw.githubusercontent.com/Tontonjo/joix/main/joix.yml
+
 # Starting joix using file
 docker-compose -f $rootconfigfolder/joix.yml -p joix up -d
 
