6ac93edf75fd8eaa3a4b6923eb94a6129b1fc921
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-08-29 13:18:58 +0200

    Update joix_startup.sh

--- joix_startup.sh
+++ joix_startup.sh
@@ -4,6 +4,8 @@
 
 # Settings
 username=joix
+rootconfigfolder=/etc/joix
+rootdatafolder=/opt/joix
 
 ## Starting Nextcloud Installation
 # Creating environnment and variables
@@ -13,12 +15,16 @@ gid=$(id -g $username)
 uid=$(id -u $username)
 
 # Creating folders and setting rights
-rootconfigfolder=/etc/joix
-rootdatafolder=/opt/joix
 mkdir $rootdatafolder
 mkdir $rootconfigfolder
 chown -R $uid:$gid $rootdatafolder
 chown -R $uid:$gid $rootconfigfolder
 
+# Getting joix.yml
+wget -O /etc/joix/joix.yml https://raw.githubusercontent.com/Tontonjo/joix/main/joix.yml
+
 # Starting joix using file
 docker compose -f /opt/joix/joix.yml -p joix up -d
+
+# Deleting this script
+rm -f $0
