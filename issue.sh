2e9d1c8c0b16c66ae97d47610c06e09df7d57bca
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-09-01 15:32:42 +0200

    Update issue.sh

--- issue.sh
+++ issue.sh
@@ -3,6 +3,7 @@
 # Tonton Jo - 2021
 sleep 10
 ip=$(hostname -I | awk '{print $1}')
+host=$(hostname)
 
 echo "
      ____.      .__        
@@ -12,4 +13,5 @@ echo "
  \________|\____/|__/__/\_ \
                           \/
 ---- Welcom to Joix OS ----
-Host IP: $ip" > /etc/issue
+Host IP:  $ip
+Hostname: $host" > /etc/issue
